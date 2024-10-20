1166c7dab2cb7d8a768defbd5221d5430a471a18
GARRY\GARRY <antgarper10@alu.edu.gva.es>
2024-10-20 19:04:08 +0200

    20/10/2024

--- ejercicios/src/main/java/org/example/Main.java
+++ ejercicios/src/main/java/org/example/Main.java
@@ -1,16 +1,16 @@
 package org.example;
-import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Tema1_actividadinicial actividades = new Tema1_actividadinicial();
+        actividades.actividad_inicial();
         //Ejercicios ejercicio = new Ejercicios();
         //ejercicio.practica3();
         //tema2_ejercicios ejercicios = new tema2_ejercicios();
         //ejercicios.ejercicio3();
-        tema2_actividad actividad = new tema2_actividad();
-        actividad.ejerciciodeampliacion();
+        //tema2_actividad actividad = new tema2_actividad();
+        //actividad.ejerciciodeampliacion();
 
     //Operadores operadores = new Operadores();
     //operadores.aritmeticos();
--- ejercicios/src/main/java/org/example/tema2_ejercicios.java
+++ ejercicios/src/main/java/org/example/tema2_ejercicios.java
@@ -1,63 +1,63 @@
-package org.example;
-import java.util.Scanner;
-import java.util.InputMismatchException;
+//package org.example;
+//import java.util.Scanner;
+//import java.util.InputMismatchException;
+//
+//public class tema2_ejercicios {
+////bien
+//    public void ejercicio1 (){
+//        Scanner entrada = new Scanner(System.in);
+//
+//    }
+//
+//    public void ejercicio3() {
+//
+//        Scanner teclado = new Scanner(System.in);
+//        boolean err = true;
+//        int n = 0;
+//        int m = 0;
+//
+//        try {
+//            System.out.printf("Introduce un número...");
+//            n = teclado.nextInt();
+//            System.out.printf("Introduce un número para quitar las cifras al anterior...");
+//            m = teclado.nextInt();
+//            if (n > m) {
+//                err = false;
+//            } else {
+//                System.out.printf("No se pueden quitar más cifras de las que hay.");
+//            }
+//
+//        } catch (InputMismatchException e) {
+//            System.out.printf("El formato no es correcto(númerico)");
+//            teclado.nextLine();
+//        }
+//
+//        String n_string = Integer.toString(n);
+//        int tamanyo = n_string.length();
+//
+//        n_string = n_string.substring(0, tamanyo - m);
+//
+//        System.out.printf("El número sin las " + m + " cifras es " + n_string);
+//
+//    }
+//
+//    public  void ejercicio4 (){
+//        Scanner entrada = new Scanner(System.in);
+//
+//        String text = "Hola Mundo, bienvenido a Mundo. Mundo es genial.";
+//
+//        String subcadena = "Mundo";
+//
+//
+//    }
+//
+////    public void ejerciciodeampliacion(){
+////        Scanner entrada = new Scanner(System.in);
+////
+////        System.out.println("¿Que altura tienes en cm?");
+////        System altura = entrada.next();
+//
+//
+//    }
 
-public class tema2_ejercicios {
-//bien
-    public void ejercicio1 (){
-        Scanner entrada = new Scanner(System.in);
 
-    }
-
-    public void ejercicio3() {
-
-        Scanner teclado = new Scanner(System.in);
-        boolean err = true;
-        int n = 0;
-        int m = 0;
-
-        try {
-            System.out.printf("Introduce un número...");
-            n = teclado.nextInt();
-            System.out.printf("Introduce un número para quitar las cifras al anterior...");
-            m = teclado.nextInt();
-            if (n > m) {
-                err = false;
-            } else {
-                System.out.printf("No se pueden quitar más cifras de las que hay.");
-            }
-
-        } catch (InputMismatchException e) {
-            System.out.printf("El formato no es correcto(númerico)");
-            teclado.nextLine();
-        }
-
-        String n_string = Integer.toString(n);
-        int tamanyo = n_string.length();
-
-        n_string = n_string.substring(0, tamanyo - m);
-
-        System.out.printf("El número sin las " + m + " cifras es " + n_string);
-
-    }
-
-    public  void ejercicio4 (){
-        Scanner entrada = new Scanner(System.in);
-
-        String text = "Hola Mundo, bienvenido a Mundo. Mundo es genial.";
-
-        String subcadena = "Mundo";
-
-
-    }
-
-    public void ejerciciodeampliacion(){
-        Scanner entrada = new Scanner(System.in);
-
-        System.out.println("¿Que altura tienes en cm?");
-        System altura = entrada.next();
-
-
-    }
-
-}
