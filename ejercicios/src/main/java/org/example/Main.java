88e7d5f4b62c9a4fe907ca8025ed7c15cf091b14
GARRY\GARRY <antgarper10@alu.edu.gva.es>
2024-10-30 22:05:44 +0100

    ..

--- ejercicios/src/main/java/org/example/Main.java
+++ ejercicios/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Tema2_ejercicios ejercicios=new Tema2_ejercicios();
-        ejercicios.repaso7();
+        ejercicios.examen();
         //Tema2 string = new Tema2();
         //string.excepciones();
         //Aleatorios aleatorios = new Aleatorios();
